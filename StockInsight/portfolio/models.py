bac8024d53042e12778f53ea12a5110bf6d3289e
Wojti9 <wjetek@gmail.com>
2025-01-06 23:19:28 +0100

    Added dashboard with default chart. Added currency view to see other currencies.

--- StockInsight/portfolio/models.py
+++ StockInsight/portfolio/models.py
@@ -1,3 +1,57 @@
 from django.db import models
+import requests
+import json
+from datetime import datetime, timedelta
 
-# Create your models here.
+class StockData(models.Model):
+    timestamp = models.DateTimeField()
+    price = models.FloatField()
+
+    objects = models.Manager()
+
+    @staticmethod
+    def fetch_and_process_data(search, window):
+        SERP_API_KEY = '3e59356712e9917d51d96eb521ace6d4f60291e1fa0b28e1237feb4d7440613c'
+        response = requests.get(f'https://serpapi.com/search.json?engine=google_finance&q={search}&window={window}&api_key={SERP_API_KEY}')
+        data = response.json()
+
+        stock_data = data.get('graph', [])
+        filtered_stock_data = []
+        last_timestamp = None
+
+        # Determine the time delta based on the window
+        time_delta = {
+            '1d': timedelta(minutes=10),
+            '5d': timedelta(hours=2),
+            '1m': timedelta(days=1),
+            '6m': timedelta(weeks=1),
+            '1y': timedelta(weeks=2),
+            '5y': timedelta(weeks=4)
+        }.get(window, timedelta(minutes=10))  # Default to 10 minutes if window is unknown
+
+        for item in stock_data:
+            timestamp = datetime.strptime(item['date'], '%b %d %Y, %I:%M %p %Z')
+            rounded_timestamp = timestamp.replace(minute=0, second=0, microsecond=0)
+            if timestamp.minute >= 30:
+                rounded_timestamp += timedelta(hours=1)
+            if last_timestamp is None or rounded_timestamp >= last_timestamp + time_delta:
+                filtered_stock_data.append({'timestamp': rounded_timestamp.isoformat(), 'price': item['price']})
+                last_timestamp = rounded_timestamp
+
+        return json.dumps(filtered_stock_data)
+
+    @staticmethod
+    def get_available_windows():
+        return ['1d', '5d', '1m', '6m', '1y', '5y']
+
+class Article(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+
+    objects = models.Manager()
+
+class Currency(models.Model):
+    name = models.CharField(max_length=50)
+    value = models.FloatField()
+
+    objects = models.Manager()
--- StockInsight/portfolio/views.py
+++ StockInsight/portfolio/views.py
@@ -1,7 +1,10 @@
 from django.contrib.auth import login
 from django.contrib.auth.forms import AuthenticationForm
 from django.shortcuts import render, redirect
+import json
 from django.contrib.auth.decorators import login_required
+from .models import StockData
+from django.http import JsonResponse
 
 
 def login_view(request):
@@ -17,6 +20,100 @@ def login_view(request):
     return render(request, 'portfolio/login.html', {'form': form})
 
 
-@login_required
 def portfolio_view(request):
     return render(request, 'portfolio/portfolio.html')
+
+
+@login_required
+def dashboard_view(request, window="1d"):
+    search = "EUR-USD"
+    stock_data = StockData.fetch_and_process_data(search, window)
+    stock_data_json = json.dumps(stock_data) if stock_data else '[]'
+    available_windows = StockData.get_available_windows()
+
+    return render(request, 'dashboard.html', {
+        'search': search,
+        'window': window,
+        'stock_data': stock_data,
+        'stock_data_json': stock_data_json,
+        'available_windows': available_windows
+    })
+
+
+@login_required
+def currency_view(request, search, window="1d"):
+    stock_data = StockData.fetch_and_process_data(search, window)
+    stock_data_json = json.dumps(stock_data) if stock_data else '[]'
+    available_windows = StockData.get_available_windows()
+
+    return render(request, 'currencies/currency.html', {
+        'search': search,
+        'window': window,
+        'stock_data': stock_data,
+        'stock_data_json': stock_data_json,
+        'available_windows': available_windows
+    })
+
+
+@login_required
+def chart_view(request, search, window):
+    stock_data = StockData.fetch_and_process_data(search, window)
+    stock_data_json = json.dumps(stock_data) if stock_data else '[]'
+
+    # Detect currency symbol
+    currency_symbol = {
+        "USD": "$",
+        "EUR": "€",
+        "GBP": "£",
+        "JPY": "¥",
+        "AUD": "A$",
+        "CAD": "C$",
+        "CHF": "CHF",
+        "CNY": "¥",
+        "NZD": "NZ$",
+        "BTC": "₿",
+        "ETH": "Ξ",
+        "XRP": "XRP",
+        "BCH": "BCH",
+        "ADA": "₳",
+        "DOT": "DOT",
+        "BNB": "BNB",
+        "USDT": "₮"
+    }.get(search.split('-')[1], "")
+
+    # Currency full names
+    currency_names = {
+        "USD": "US Dollar",
+        "EUR": "Euro",
+        "GBP": "British Pound",
+        "JPY": "Japanese Yen",
+        "AUD": "Australian Dollar",
+        "CAD": "Canadian Dollar",
+        "CHF": "Swiss Franc",
+        "CNY": "Chinese Yuan",
+        "NZD": "New Zealand Dollar",
+        "BTC": "Bitcoin",
+        "ETH": "Ethereum",
+        "LTC": "Litecoin",
+        "XRP": "Ripple",
+        "BCH": "Bitcoin Cash",
+        "ADA": "Cardano",
+        "DOT": "Polkadot",
+        "BNB": "Binance Coin",
+        "USDT": "Tether"
+    }
+
+    # Generate chart label
+    currencies = search.split('-')
+    if len(currencies) == 2:
+        from_currency = currency_names.get(currencies[0], currencies[0])
+        to_currency = currency_names.get(currencies[1], currencies[1])
+        chart_label = f'{from_currency} to {to_currency}'
+    else:
+        chart_label = f'Trending {search}'
+
+    return render(request, 'chart.html', {
+        'stock_data': stock_data_json,
+        'chart_label': chart_label,
+        'currency_symbol': currency_symbol
+    })
