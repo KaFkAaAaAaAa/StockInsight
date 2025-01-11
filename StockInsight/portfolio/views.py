c24a5f2190fa76012d9a01fdc4d8cdd5a78cba6e
Wojti9 <wjetek@gmail.com>
2025-01-11 18:58:23 +0100

    Remade login form and added error display on the pages

--- StockInsight/portfolio/views.py
+++ StockInsight/portfolio/views.py
@@ -1,9 +1,9 @@
-from django.contrib.auth import login
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login, authenticate
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .models import StockData
 from .forms import RegisterForm
+from .forms import LoginForm
 from django.http import JsonResponse
 
 from urllib.parse import urljoin
@@ -12,17 +12,18 @@ import requests
 from bs4 import BeautifulSoup
 from urllib.parse import urlparse
 
-
 def login_view(request):
-    if request.method == "POST":
-        form = AuthenticationForm(request, data=request.POST)
+    if request.method == 'POST':
+        form = LoginForm(request, data=request.POST)
         if form.is_valid():
-            user = form.get_user()
-            login(request, user)
-            return redirect('portfolio')
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('dashboard')
     else:
-        form = AuthenticationForm()
-
+        form = LoginForm()
     return render(request, 'portfolio/login.html', {'form': form})
 
 def register_view(request):
